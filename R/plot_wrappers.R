7d7b3c57d4f2170bf9de07536b24dc1d2c16151b
Alex Chubaty <achubaty@for-cast.ca>
2024-06-18 14:01:32 -0600

    ggplot wrappers for scfm rasters

--- R/plot_wrappers.R
+++ R/plot_wrappers.R
@@ -17,6 +17,87 @@ plot_fireRegimePolys <- function(fireRegimePolys) {
 
   ggplot(fireRegimePolys) +
     geom_sf(aes(fill = PolyID)) +
-    scale_fill_discrete() +
-    theme_minimal()
+    scale_fill_discrete() + ## TODO: use same palette as plot_fireRegimeRas ??
+    theme_bw()
+}
+
+#' Plot fire regime raster
+#'
+#' @param x `SpatRaster` object corresponding to a fire regime raster
+#'
+#' @param title character, the plot title
+#'
+#' @return `ggplot` object
+#'
+#' @export
+#' @importFrom ggplot2 ggplot ggtitle scale_fill_brewer theme_bw
+#' @importFrom tidyterra geom_spatraster
+plot_fireRegimeRas <- function(x, title) {
+  ggplot() +
+    geom_spatraster(data = terra::as.factor(x)) +
+    scale_fill_brewer(palette = "Paired", type = "qual", na.value = "transparent") +
+    ggtitle(title) +
+    theme_bw()
+}
+
+#' Plot age map
+#'
+#' @param x `SpatRaster` object corresponding to stand age or time since disturbance map
+#'
+#' @param title character, the plot title
+#'
+#' @param maxAge the maximum age to plot
+#'
+#' @return `ggplot` object
+#'
+#' @export
+#' @importFrom ggplot2 ggplot ggtitle scale_fill_distiller theme_bw
+#' @importFrom tidyterra geom_spatraster
+plot_ageMap <- function(x, title, maxAge) {
+  x[x > maxAge] <- maxAge
+
+  ggplot() +
+    geom_spatraster(data = x) +
+    scale_fill_distiller(palette = "Greens", direction = 1, na.value = "transparent") +
+    ggtitle(title) +
+    theme_bw()
+}
+
+#' Plot burn maps
+#'
+#' @param x `SpatRaster` object corresponding to a current or cumulative burn map.
+#'
+#' @param title character, the plot title
+#'
+#' @return `ggplot` object
+#'
+#' @export
+#' @importFrom ggplot2 ggplot ggtitle theme_bw
+#' @importFrom tidyterra geom_spatraster
+#' @importFrom viridis scale_fill_viridis
+plot_burnMap <- function(x, title) {
+  ggplot() +
+    geom_spatraster(data = x) +
+    scale_fill_viridis(na.value = "transparent") +
+    ggtitle(title) +
+    theme_bw()
+}
+
+#' Plot flammable map
+#'
+#' @param x `SpatRaster` object corresponding to a flammability map.
+#'
+#' @param title character, the plot title
+#'
+#' @return `ggplot` object
+#'
+#' @export
+#' @importFrom ggplot2 ggplot ggtitle scale_fill_distiller theme_bw
+#' @importFrom tidyterra geom_spatraster
+plot_flammableMap <- function(x, title) {
+  ggplot() +
+    geom_spatraster(data = x) +
+    scale_fill_distiller(palette = "RdBu", na.value = "transparent") +
+    ggtitle(title) +
+    theme_bw()
 }
