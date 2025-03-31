dd23a94c11d6aa1c6bc8c082ed32c80238f99503
ianmseddy <ianmseddy@gmail.com>
2025-03-31 14:57:31 -0700

    stop if pEscape is above 1

--- R/utils_scfmRegime.R
+++ R/utils_scfmRegime.R
@@ -130,6 +130,10 @@ calcZonalRegimePars <- function(polygonID, firePolys, firePoints, epochLength, m
   paramData <- cbind(ignitionRate, pEscape, xBar, lxBar, xMax, emfs_ha, empiricalBurnRate) |>
     as.data.table()
   paramData$PolyID <- polygonID
+
+  if (pEscape > 1) {
+    stop("pEscape exceeds 1, which should not be possible")
+  }
   ## rate - per ha/per year ; pEscape; xBar - mean fire size; lxBar - mean log;
   ## xMax - maximum observed size; #emfs_ha - Estiamted maximum Fire Size in ha
   ## max fire size is returned twice - I think this is a backwards compatibility decision
